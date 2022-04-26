e16c51e3c3cfd8abb52dd44df4b61f5500c33993
Your Name <you@example.com>
2022-04-26 14:27:33 +0800

    20220426

---
+++ pages/refund/refund.js
@@ -0,0 +1,146 @@
+// pages/refund/refund.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgs: [],
+    array: ['我要退货退款', '我要退款(无需退货)'],
+    index: 0,
+    array1: ['请选择申请原因', '不喜欢、效果不好', '多拍、错拍、不想要', '货物与描述不符', '质量问题', '商家发错货', '其他'],
+    index1: 0,
+  },
+  bindPickerChange: function(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value
+    })
+  },
+  
+  bindPickerChange1: function(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index1: e.detail.value
+    })
+  },
+
+  // 上传图片
+chooseImg: function (e) {
+  var that = this;
+  var imgs = this.data.imgs;
+  if (imgs.length >= 9) {
+    this.setData({
+      lenMore: 1
+    });
+    setTimeout(function () {
+      that.setData({
+        lenMore: 0
+      });
+    }, 2500);
+    return false;
+  }
+  wx.chooseImage({
+    count: 4, // 默认9
+    sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+    sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+    success: function (res) {
+      // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+      var tempFilePaths = res.tempFilePaths;
+      var imgs = that.data.imgs;
+      // console.log(tempFilePaths + '----');
+      for (var i = 0; i < tempFilePaths.length; i++) {
+        if (imgs.length >= 9) {
+          that.setData({
+            imgs: imgs
+          });
+          return false;
+        } else {
+          imgs.push(tempFilePaths[i]);
+        }
+      }
+      // console.log(imgs);
+      that.setData({
+        imgs: imgs
+      });
+    }
+  });
+},
+// 删除图片
+deleteImg: function (e) {
+  var imgs = this.data.imgs;
+  var index = e.currentTarget.dataset.index;
+  imgs.splice(index, 1);
+  this.setData({
+    imgs: imgs
+  });
+},
+// 预览图片
+previewImg: function (e) {
+  //获取当前图片的下标
+  var index = e.currentTarget.dataset.index;
+  //所有图片
+  var imgs = this.data.imgs;
+  wx.previewImage({
+    //当前显示图片
+    current: imgs[index],
+    //所有图片
+    urls: imgs
+  })
+},
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
